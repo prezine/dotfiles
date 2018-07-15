b37d5e5e91bb01190e55ecee7af8b88c1bb1a45b
Precious Tom <tomprezine@gmail.com>
2018-07-15 09:37:28 +0100

    Update dot.bash

--- dot.bash
+++ dot.bash
@@ -8,4 +8,33 @@ upower -i /org/freedesktop/UPower/devices/battery_BAT0
 `# Open Google Chrome `\
 /usr/bin/google-chrome
 
-`# Run sudo su with password segment `\
+`# Installables `\
+
+# apt-get maintainace
+apt-get update
+
+apt-get install node
+
+## Makes adding SSH keys to servers easier.
+apt-get install ssh-copy-id
+
+#apt-get install docker boot2docker
+#apt-get install boot2docker
+
+# Install kubernetes
+apt-get install kubernetes-cli
+
+apt-get install gcc
+apt-get install git
+apt-get install gpg
+apt-get install openssh
+apt-get install tmux
+apt-get install go
+apt-get install node
+
+# Cli tools
+apt-get install tree
+apt-get install wget
+apt-get install nmap
+
+apt-get install zsh
